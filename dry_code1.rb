120ff80bed1256e3047ae5804eb7d16fbb90a5e7
Enow Mbi <benowmbi@yahoo.com>
2019-05-29 11:32:46 +0100

    step 2 of code

--- dry_code1.rb
+++ dry_code1.rb
@@ -2,27 +2,38 @@ class User
   attr_accessor :valid,:balance,:authorized
 
   def user_balance
-    if valid && authorized && balance > 0
+    if account_valid?
       puts balance
     else
-      puts "Not a valid Account" 
+      warning_account_not_valid
     end
   end
 
   def user_profile
-    if valid && authorized && balance > 0
-      puts "Profile information goes here !"
+    if account_valid?  
+    puts "Profile information goes here !"
     else
-      puts "Not a valid Account" 
+      warning_account_not_valid
     end
   end
 
   def add_funds(amount)
-    if valid && authorized && balance > 0
+    if account_valid?
      balance = balance + amount
     else
-      puts "Not a valid Account" 
+      warning_account_not_valid
     end
   end
 
+  def account_valid
+    if valid && authorized && balance > 0
+      return true
+    else
+      false
+    end
+  end
+
+  def warning_account_not_valid
+      puts "Not a valid Account" 
+  end
 end
