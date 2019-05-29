07190e38552f1c2cc312189a81d7a63d77bf490b
Enow Mbi <benowmbi@yahoo.com>
2019-05-29 11:24:17 +0100

    default code to be dry-ed

--- dry_code1.rb
+++ dry_code1.rb
@@ -1,19 +1,28 @@
 class User
-  attr_accessor :valid,:balance,:authorized,:age,:zip_code
+  attr_accessor :valid,:balance,:authorized
 
   def user_balance
-
-
+    if valid && authorized && balance > 0
+      puts balance
+    else
+      puts "Not a valid Account" 
+    end
   end
 
   def user_profile
-
-
+    if valid && authorized && balance > 0
+      puts "Profile information goes here !"
+    else
+      puts "Not a valid Account" 
+    end
   end
 
-  def add_fund(amount)
-
-
+  def add_funds(amount)
+    if valid && authorized && balance > 0
+     balance = balance + amount
+    else
+      puts "Not a valid Account" 
+    end
   end
 
 end
