80eeb7a4238cb90ef460b3d35d84648e40c12bc8
Enow Mbi <benowmbi@yahoo.com>
2019-09-08 20:23:25 +0100

    Add accessor2

---
+++ accessor2.rb
@@ -0,0 +1,29 @@
+module Accessor
+  def my_attr_accessor name
+    ivar_name = "@#{name}"
+    #getter
+    define_method(name) do 
+      instance_variable_get(ivar_name)
+    end
+
+    #setter
+    define_method("#{name}=") do |value|
+      instance_variable_set(ivar_name,value)
+    end
+  end
+end
+
+
+class Thing
+  extend Accessor
+  attr_accessor :name, :address
+end
+
+th = Thing.new
+th.name ="Enow Mbi"
+th.address ="Bomaka,Buea"
+puts th.name
+puts th.address
+
+p th.methods
+
