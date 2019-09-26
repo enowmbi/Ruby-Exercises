56cb0948105df3cded38c0ccf0799dc69a29b29b
Enow Mbi <benowmbi@yahoo.com>
2019-09-26 08:40:42 +0100

    Modify prime_numbers

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -1,21 +1,24 @@
 require 'benchmark'
 
 result = Benchmark.measure do 
-x = 1
-prime =[]
-max = 500
+  x = 1
+  prime =[]
+  max = 20
+  y = 1
+  until x > max
+    while y < max/2
+      #if x == 1 || x == 2 || x == 3 || x == 5 || x == 7
+      if x % y !=0
+        puts x is prime prime << x
+      elsif x % 2 > 0 && x % 3 > 0 && x % 5 > 0 && x % 7 > 0
+        prime << x
+      end  
+      y+=1
+    end
 
-until x > max
-  if x == 1 || x == 2 || x == 3 || x == 5 || x == 7
-   prime << x
-  elsif x % 2 > 0 && x % 3 > 0 && x % 5 > 0 && x % 7 > 0
-     prime << x
-  end  
-  x+=1
-
-end
-
-p "Prime numbers from 1 to #{max} are #{prime}"
+    x+=1
+  end
+  p "Prime numbers from 1 to #{max} are #{prime}"
 
 end
 p result
