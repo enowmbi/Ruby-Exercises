2f289a041094d147d93897083b9210517d14f5f7
Enow Mbi <benowmbi@yahoo.com>
2019-09-30 12:30:35 +0100

    Add exponentiation

--- exponentiation.rb
+++ exponentiation.rb
@@ -1,1 +1,22 @@
-#Exponentiation
+=begin 
+Exponentiation
+x^n = x * x^n-1
+=end
+
+def pow(x,n)
+
+  return 1 if n == 0
+
+  return x * pow(x,n-1)
+
+end
+
+
+
+
+
+puts pow(2,10)
+
+puts pow(3,10)
+
+puts pow(5,10)
