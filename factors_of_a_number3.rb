f4e5041490461987373e6009b3d51e79bae689d3
Enow Mbi <benowmbi@yahoo.com>
2019-09-30 13:19:35 +0100

    Add factors of a number - good algorithm. from 1 to n-1 to get factors

---
+++ factors_of_a_number3.rb
@@ -0,0 +1,23 @@
+def factors_of_number(n)
+
+ factors = [1]  # initialize array with the first factor 1 because 1 is a factor of n
+ 
+ 2.upto(n-1) do |i|
+  
+ if n % i == 0
+
+    factors << i
+
+  end
+ end
+  factors << n   # add number itself because n is a factor of n
+ return "Factors of #{n} are #{factors}"
+end
+
+
+
+puts factors_of_number(10)
+
+puts ""
+
+puts factors_of_number(20)
