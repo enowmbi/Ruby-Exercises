0b45d38f2237641a33c5462a0a8e9703a454e76f
Enow Mbi <benowmbi@yahoo.com>
2019-10-03 13:22:10 +0100

    Modify sieve of eratosthenes

--- sieve_of_eratosthenes.rb
+++ sieve_of_eratosthenes.rb
@@ -8,26 +8,6 @@ iterate through the numbers from i = 2 to i = n
 if the primes[i] == true or 1 
 set its values for its multiples in the primes array to false or 0 i.e. i*j < n
 
-primes <= [n]
-for i <- 2 to i <- n 
-  primes[i] = 1
- end
-
-primes[0] = 0
-primes[1] = 0
-
-
-    for i = 0 to i < n
-        if primes[i] == 1
-    for j <- i + 1  to j < i*
-
-end
-
-=end
-
-=begin
-  We use the sieve of Eratosthenes to obtain the prime numbers up to a given a number n
-  
 steps
   - iterate through all numbers from 0 or 1 or 2 to number and create an array and assign each the value of true (1)
   - set 1 and 0 to false(0) since they are not prime numbers - do this only if you started the previous step from 0
@@ -38,23 +18,45 @@ steps
 =end
 
 def sieve_of_eratosthenes(number)
- primes = []
 
- 0.upto(n) do |i|
+ primes = []
+ prime_numbers = []
+
+ 0.upto(number) do |i|
   primes[i] = 1
  end
+
  primes[0] = 0
  primes[1] = 0
 
- 2.upto(n) do |j|
-  if primes[i] == 1
-    (k+1).upto(
+ 2.upto(Math.sqrt(number)) do |j|
+  if primes[j] == 1
+     2.upto(number) do |k|
+      if j*k <= number 
+        primes[(j*k)] = 0
+      end
+     end
+  end
+ end
 
+ primes.each_with_index do |value,index|
+   if value == 1
+      prime_numbers << index
+   end
+ end
 
+ puts "prime numbers up to #{number} are #{prime_numbers}"
 
 end
 
 
+ sieve_of_eratosthenes(10)
 
+ sieve_of_eratosthenes(25)
 
+ sieve_of_eratosthenes(50)
+
+ sieve_of_eratosthenes(75)
+
+ sieve_of_eratosthenes(100)
 
