22ebb78d790a11a15b2da841916fe6d93d0b7295
Enow Mbi <benowmbi@yahoo.com>
2019-10-09 01:26:45 +0100

    Modify factorial algorithm

--- algorithms_and_pseudocode/factorial.rb
+++ algorithms_and_pseudocode/factorial.rb
@@ -1,9 +1,6 @@
 def factorial(n)
-  if n == 0
-    1
-  else
-    n * factorial(n-1)
-  end
+  return 1 if n == 0
+  return n * factorial(n-1)
 end
 
 puts "Displaying value of 10! = #{factorial(10)}"
