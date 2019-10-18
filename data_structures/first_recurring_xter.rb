61360f0f3ee21731351e8e477f316beecae5754a
Enow Mbi <benowmbi@yahoo.com>
2019-10-18 04:42:31 +0100

    Change file5.rb to first_recurring_xter

---
+++ data_structures/first_recurring_xter.rb
@@ -0,0 +1,14 @@
+=begin 
+ given a series of xters - write an algorithm that returns the first recurring character
+ solution
+ use a hash table 
+=end
+
+def first_recurring_xter(string)
+ # take string and split 
+
+
+
+
+
+end
