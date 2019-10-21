d9a145d2019dcaee3e1782c604f50639865bc017
Enow Mbi <benowmbi@yahoo.com>
2019-10-21 21:31:34 +0100

    Rename file7.rb to largest_range.rb

--- data_structures/file7.rb
+++
@@ -1,3 +0,0 @@
-=begin 
-this is file 7
-=end
---
+++ data_structures/largest_range.rb
@@ -0,0 +1,12 @@
+=begin 
+Given a list of positive numbers [1,11,3,0,15,5,2,4,10,7,12,6], find the largest range of [consecutive] numbers
+=end
+
+
+def largest_range(list_of_numbers)
+
+end
+
+
+list = [1,11,3,0,15,5,2,4,10,7,12,6]
+puts largest_range(list)
