d3f6c753d81b06ca9bdf12188a6ca4e47aae5c22
Enow Mbi <benowmbi@yahoo.com>
2019-10-22 15:57:42 +0100

    Modify largest range

--- data_structures/largest_range.rb
+++ data_structures/largest_range.rb
@@ -1,9 +1,16 @@
 =begin 
 Given a list of positive numbers [1,11,3,0,15,5,2,4,10,7,12,6], find the largest range of [consecutive] numbers
+
+In this solution, we'll use a hash to store all values
+start with the first entry
+check for values to the left (lower than it) if they are in the array
+check for values to the right of it if they are in the array 
+if values are not found move to next number in the hash
 =end
 
 
 def largest_range(list_of_numbers)
+
 
 end
 
