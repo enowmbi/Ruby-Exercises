8b7adb5265e5d7a5a731666f37782f0d35ddf015
Enow Mbi <benowmbi@yahoo.com>
2019-11-11 20:13:59 +0100

    Modify fibonacci with memoization

--- fibonacci_with_memoization.rb
+++ fibonacci_with_memoization.rb
@@ -1,36 +1,23 @@
-=begin
- Use memoization to improve performance of this algorithm - reducing the computational complexity
- create an array to hold values 
- for any computation - store the value in the variables
-=end
-
-def fibonacci(n)
-
-  fibs = {}
-
-  if n == 0
-    fibs[0] = 0
-    return fibs[0]
-  end
-
-  if n == 1
-    fibs[1] = 1
-    return fibs[1]
-  end
-
-  if fibs[n]
-    return fibs[n]
-  else
-    fibs[n] = fibonacci(n - 1) + fibonacci(n - 2)
-    return fibs[n]
-  end
-
+def fib(number, memo =[])
+ if number <= 1
+   return number
+ else
+   if memo[number]
+     return memo[number]
+   else
+     memo[number] = fib(number - 1,memo) + fib(number - 2 ,memo)
+     return memo[number]
+   end
+ end
 end
 
 
-require 'benchmark.rb'
-report = Benchmark.measure do 
-  puts fibonacci(40)
-end
-puts report
+
+puts fib(5)
+puts fib(10)
+puts fib(20)
+puts fib(30)
+puts fib(40)
+puts fib(100)
+puts fib(1000)
 
