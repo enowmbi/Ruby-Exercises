33a15a4d3b14487d9170c77766a3d540c66f1448
Enow Mbi <benowmbi@yahoo.com>
2019-11-11 20:29:54 +0100

    Modify fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,6 @@
 def fibonacci(number)
- if number == 0 || number == 1
-   return number 
- else
+ return number if number <= 1 
  return fibonacci(number - 1) + fibonacci(number - 2)
- end
 end
 
 require 'benchmark.rb'
