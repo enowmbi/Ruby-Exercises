bb67f64e6768d2f412b3d70be82a10152bf31a30
Enow Mbi <benowmbi@yahoo.com>
2019-11-11 20:29:33 +0100

    Modify fib with memo

--- data_structures/fibonacci_with_memoization.rb
+++ data_structures/fibonacci_with_memoization.rb
@@ -13,16 +13,16 @@ end
 
 def fibonacci2(number)
  return number if number <= 1
- return fibonacci(number - 1) + fibonacci(number - 2) 
+ return fibonacci2(number - 1) + fibonacci2(number - 2) 
 end
 
 require 'benchmark'
 report = Benchmark.measure do 
-puts "#{fibonacci(100)} - using memoization"
+puts "#{fibonacci(1000)} - using memoization"
 end
 puts report
 
 report2 = Benchmark.measure do 
-puts "#{fibonacci2(100)} - not using memoization"
+puts "#{fibonacci2(40)} - not using memoization"
 end
 puts report2
