f4b491802edc41c09d5f075b9e8f351c303ac12d
Enow Mbi <benowmbi@yahoo.com>
2019-11-20 08:43:58 +0100

    Add digital pet as the class that starts the digital pet stimulator

---
+++ object_oriented_programming_projects/state_diagrams/digital_pet/digital_pet.rb
@@ -0,0 +1,10 @@
+require_relative 'pet.rb'
+require_relative 'owner.rb'
+
+owner = Owner.new("Max")
+owner.punish_pet
+owner.praise_pet
+owner.praise_pet
+owner.punish_pet
+owner.punish_pet
+owner.praise_pet
