922ac878739fa9d07eaccc820b1cb1864df3e7ac
Enow Mbi <benowmbi@yahoo.com>
2019-11-20 10:06:46 +0100

    Modify digital pet to allow user to make selections

--- object_oriented_programming_projects/state_diagrams/digital_pet/digital_pet.rb
+++ object_oriented_programming_projects/state_diagrams/digital_pet/digital_pet.rb
@@ -1,10 +1,29 @@
 require_relative 'pet.rb'
 require_relative 'owner.rb'
 
+
+puts "Welcome to ACME Digital Pet simulator".center(50,'*')
+puts "=".center(50,'=')
+
+puts "Select '0' to Revert to initial state of pet or  '1' to Punish pet or '2' to Praise or 'x' to exit."
+puts "Press 'Enter' after each selection"
 owner = Owner.new("Max")
-owner.punish_pet
-owner.praise_pet
-owner.praise_pet
-owner.punish_pet
-owner.punish_pet
-owner.praise_pet
+entry = ""
+until entry == 'x' do 
+  puts "Please select an option"
+  entry = gets.chomp()
+  if entry =='0'
+    puts "Reverting to initial state".rjust(50,'*')
+    owner = Owner.new('Max')
+  end
+  if entry == '1'
+    owner.punish_pet()
+  end
+  if entry == '2'
+    owner.praise_pet()
+  end
+
+end
+puts "*".center(50,'*')
+puts "Thank you for using ACME Digital Pet simulator"
+
