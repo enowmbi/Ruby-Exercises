4f2ff6bb33eb89937537ee217024328a6e772178
Enow Mbi <benowmbi@yahoo.com>
2019-11-20 08:44:35 +0100

    Add pet with all code

---
+++ object_oriented_programming_projects/state_diagrams/digital_pet/pet.rb
@@ -0,0 +1,49 @@
+class Pet 
+
+  def initialize(name)
+    @state = "Happy"
+    @name = name
+    puts "#{@name} is #{@state}"
+  end
+
+=begin
+  def receive_stimuli(stimuli)
+    if @state == "Happy" && stimuli == "Punishment"
+      @state = "Sad"
+    elsif @state == "Sad" && stimuli == "Punishment"
+      @state = "Heart Broken"
+    elsif @state == "Sad" && stimuli =="Punishment"
+      @state = "Heart Broken"
+    elsif @state == "Sad" && stimuli == "Praise"
+      @state = "Happy"
+    end
+  end
+
+=end
+
+  def receive_punishment
+    if @state == "Happy"
+      @state = "Sad"
+    elsif @state == "Sad" 
+      @state = "Heart Broken"
+    elsif @state == "Sad"
+      @state = "Heart Broken"
+    end
+  end
+
+  def receive_praise
+    if @state == "Sad" 
+      @state = "Happy"
+    end
+  end
+
+  def get_state
+    return @state
+  end
+
+  def get_name
+    return @name
+  end
+
+end
+
