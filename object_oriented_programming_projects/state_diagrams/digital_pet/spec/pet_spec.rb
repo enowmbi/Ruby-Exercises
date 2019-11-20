ca2704372310d17953eff9a5d0a4808512318303
Enow Mbi <benowmbi@yahoo.com>
2019-11-20 05:39:56 +0100

    Modify pet spec

--- object_oriented_programming_projects/state_diagrams/digital_pet/spec/pet_spec.rb
+++ object_oriented_programming_projects/state_diagrams/digital_pet/spec/pet_spec.rb
@@ -1,3 +1,26 @@
-# require_relative 'pet'
+require_relative '../pet.rb'
+
+RSpec.describe Pet do 
+
+  describe 'attributes' do 
+
+    it "doesn't allow state to be modifed from outside pet class"
+
+    it "doesn't allow name to be modified from outside pet class"
+
+    it "allows state to be modified by internal method"
+
+    it "ensures that name is a read only property from outside pet class"
+
+    it "ensures proper output when pet is initialized"
+
+    it "ensures pet can receive punishment"
+
+    it "ensures pet can receive praise"
+
+  end
+
+end
+
 
 
