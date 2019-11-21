0390cd75bf1355fbebdbace55f0538c88440032a
Enow Mbi <benowmbi@yahoo.com>
2019-11-21 12:04:54 +0100

    Add examples to owner spec file

--- object_oriented_programming_projects/state_diagrams/digital_pet/spec/owner_spec.rb
+++ object_oriented_programming_projects/state_diagrams/digital_pet/spec/owner_spec.rb
@@ -2,11 +2,22 @@ require_relative '../owner.rb'
 
 RSpec.describe Owner do 
 
-
-
-  it 'bange' do
-    expect("Bange").to be("Banige")
+describe 'methods' do 
+  before(:each) do 
+    @owner = Owner.new("Max")
   end
+
+  it "allows owner to punish pet" do 
+    expect(@owner.punish_pet).to eq("Gobacheve is Sad".rjust(50,'*'))
+    expect(@owner.punish_pet).to eq("Gobacheve is Heart Broken".rjust(50,'*'))
+  end
+
+  it "allows owner to praise pet" do
+     expect(@owner.punish_pet).to eq("Gobacheve is Sad".rjust(50,'*'))
+     expect(@owner.praise_pet).to eq("Gobacheve is Happy".rjust(50,'*'))
+     expect(@owner.praise_pet).to eq("Gobacheve is Happy".rjust(50,'*'))
+  end
+end
 
 
 end
